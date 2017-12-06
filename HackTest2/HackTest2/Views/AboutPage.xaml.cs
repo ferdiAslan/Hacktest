44601ce2407d48a9dfbb968ee70e5a598c5709a2
DIR\ferdi.aslan <ferdi.aslan@accenture.com>
2017-12-06 21:54:34 +0300

    Add project files.

---
+++ HackTest2/HackTest2/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HackTest2
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
