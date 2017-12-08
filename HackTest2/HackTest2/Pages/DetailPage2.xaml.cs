55b493403805793f0d92193421e9eeca30ba047d
DIR\ferdi.aslan <ferdi.aslan@accenture.com>
2017-12-09 02:55:10 +0300

    dadsad

--- HackTest2/HackTest2/Pages/DetailPage2.xaml.cs
+++ HackTest2/HackTest2/Pages/DetailPage2.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +14,23 @@ namespace HackTest2.Pages
 		public DetailPage2 ()
 		{
 			InitializeComponent ();
+		}
+		protected async override void OnAppearing()
+		{
+			base.OnAppearing();
+			await Task.Delay(1000);
+			UpdateUI();
+		}
+
+		private async void UpdateUI()
+		{
+			Bulut.IsVisible = true;
+			await Task.Delay(500);
+			buton1.IsVisible = true;
+			await Task.Delay(500);
+			buton2.IsVisible = true;
+			await Task.Delay(500);
+			buton3.IsVisible = true;
 		}
 	}
 }
---
+++ HackTest2/HackTest2/data/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackTest2.data
+{
+    public static class Constants
+	{
+		public const string API_URL = "https://api.motormate.abb.com/v5_0";
+		public const string API_AUTH_KEY = "TlVZeU1rRkZRVEl0T1RrME1pMDBSa1ZDTFVFeU5rUXRRME01UTBNNVEwUkZNamcz";
+	}
+}
